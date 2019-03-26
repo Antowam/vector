ac9f25b30e4cd8a3eac4ef6647bdf0c23bef647f
Anton Skatt <k8asnakeload@gmail.com>
2019-03-26 13:48:59 +0100

    Add files via upload

---
+++ vector.h
@@ -0,0 +1,53 @@
+#pragma once
+
+template <class  T>
+class vector
+{
+public:
+
+	// Constructors
+	vector<T>();
+	vector(const vector&); // copy constructor
+
+	// Destuctor
+	~vector() { delete[] vector; };
+
+	// Operator [], return element at index
+	T&	operator[](int i) { return data[i]; };
+	T	at(int i) const { return data[i] } ;
+
+	void push_back(T val);
+	void insert(T val);
+	void for_each(T val);
+
+private:
+	T* data;
+
+	int size,capacity;
+};
+
+template <typename T>
+void vector<T>::push_back(T val)
+{
+	if (size + 1 > capacity)
+		capacity *= 2;
+
+	T* tmp = data;
+	data = new T[capacity];
+
+	for (int i = 0; i < size; i++)
+	{
+		data[i] = temp[i];
+	}
+	delete temp;
+
+	return data;
+}
+
+template <typename T>
+vector<T>::vector<T>()
+{
+	capacity = 20;
+	vec = new T[capacity];
+	size = 0;
+}
