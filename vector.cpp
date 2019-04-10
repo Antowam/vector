8e2c1645eeed083b9e1a6dae16bac33e36e384fe
Anton Skatt <k8asnakeload@gmail.com>
2019-04-10 20:53:48 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -6,30 +6,15 @@ int main()
 {
 	int hello = 1;
 
-	Vector<int> vec;
+	vector<int> vec;
 
-	for (int i = 0; i < 30; i++)
+	for (int i = 0; i < 20; i++)
 	{
-		vec.PushBack(i);
-	}
-	
-	vec.Insert(3, 5);
-
-	for (int i = 0; i < vec.Size(); i++)
-	{
+		vec.Push_back(i);
 		std::cout << vec[i] << ", " << vec.Size() << ", " << vec.Max() << std::endl;
 	}
 
-	vec[0] = 45;
 
-	std::cout << vec.Empty() << std::endl;
-	std::cout << vec.Last() << std::endl;
-	std::cout << vec.First() << std::endl;
-
-	vec.Clear();
-
-	std::cout << vec.Size() << std::endl;
-
-	std::cin.get();
+	std::cin >> hello;
 
 }
