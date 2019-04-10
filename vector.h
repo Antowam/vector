681ca4b63d448ac06a57b77a0852be691c8c87a7
Anton Skatt <k8asnakeload@gmail.com>
2019-04-10 20:54:15 +0200

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,83 +1,89 @@
 #pragma once
 
 template <class  T>
-class Vector
+class vector
 {
 private:
 	T* data = nullptr;
 
-	int size, capacity;
+	int size, capacity = 0;
 
 public:
 
 	// Constructors
-	Vector<T>()
-		: capacity(20)
-		, size(0)
-	{
-		data = new T[capacity];
-	}
-
-	Vector<T>(int i)
-		: capacity(i)
-		, size(0)
-	{
-		data = new T[capacity];
-	}
-
-	Vector(const Vector& vec)
-		: capacity(vec.capacity)
-	{
-		data = new T[capacity];
-
-		for (int i = 0; size < vec.size; i++)
-		{
-			data[i] = vec.data[i];
-			size++;
-		}
-	}
+	vector<T>();
+	vector<T>(int i);
+	vector(const vector& vec);
 
 	// Destructor
-	~Vector() { delete[] data; };
+	~vector() { delete[] data; };
 
 	// Operator[], return element at index
 	T& operator[](int i) const { return data[i]; };
 
 	// Getters
-	int Clear() { return size = 0; }
-	int Max() const { return capacity; }
-	int Size() const { return size; }
-	int Last() const { return data[size - 1]; }
-	int First() const { return data[0]; }
-	bool Empty() const { return size == 0; }
+	int Max()   const { return capacity; }
+	int Size()  const { return size; };
+	T Last()  const { return data[size-1]; };
+	T First() const { return data[0]; };
+	bool Empty() const { return size == 0; };
 
 	// Mutators
-	void PushBack(T val);
-	void Insert(int i, T val);
+	void Clear() const { size = 0; };
+	void Push_back(const T& val);
+	void Insert(const T& val, int i);
 
 private:
-	void AllocNew();
+	void Alloc_new();
 };
 
 template <typename T>
-void Vector<T>::PushBack(T val)
+vector<T>::vector<T>()
+	: capacity(20)
+	, size(0)
 {
-	if (size >= capacity)
-		AllocNew();
-
-	data[size] = val;
-	size++;
+	data = new T[capacity];
 }
 
 template <typename T>
-void Vector<T>::AllocNew()
+vector<T>::vector(int i)
+	: capacity(i)
+	, size(0)
+{
+	data = new T[capacity];
+}
+
+template <typename T>
+vector<T>::vector(const vector& vec)
+	: capacity(vec.capacity)
+{	
+	data = new T[capacity];
+
+	for (int i = 0; size < vec.size; i++)
+	{
+		data[i] = vec.data[i];
+		size++;
+	}
+}
+
+template <typename T>
+void vector<T>::Push_back(const T& val)
+{
+	if (size > capacity)
+		Alloc_new();
+
+	data[size++] = val;
+}
+
+template <typename T>
+void vector<T>::Alloc_new()
 {
 	capacity *= 2;
 
 	T* tmp = data;
 	data = new T[capacity];
 
-	for (int i = 0; i < capacity; i++)
+	for (int i = 0; i < size; i++)
 	{
 		data[i] = tmp[i];
 	}
@@ -85,13 +91,20 @@ void Vector<T>::AllocNew()
 }
 
 template <typename T>
-void Vector<T>::Insert(int index, T val)
+void vector<T>::Insert(const T& val, int index)
 {
-	if (size >= capacity)
-		AllocNew();
+	if (size + 1 > capacity)
+		Alloc_new();
 
-	data[size] = data[index];
-	data[index] = val;
-
-	size++;
+	for (int i = capacity; i > index; i--)
+	{
+		if (i > index)
+			data[i] = data[i] + 1;
+		if (int i = index)
+		{
+			data[i +1] = val;
+			size++;
+			break;
+		}
+	}
 }
