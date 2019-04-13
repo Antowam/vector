20068a0e330ff086c77f1fee5353804a0a8a6c73
Anton Skatt <k8asnakeload@gmail.com>
2019-04-13 21:59:43 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,20 +1,25 @@
 #include "pch.h"
 #include <iostream>
-#include "vector.h"
+#include "Vector.h"
 
 int main()
 {
 	int hello = 1;
 
-	vector<int> vec;
+	Vector<int> vec;
 
-	for (int i = 0; i < 20; i++)
+	for (int i = 0; i < 25; i++)
 	{
-		vec.Push_back(i);
-		std::cout << vec[i] << ", " << vec.Size() << ", " << vec.Max() << std::endl;
+		vec.push_back(i);
+	}
+	
+	vec.swap(vec[5], vec[22]);
+	vec.sort();
+
+	for (int i = 0; i < vec.length(); i++)
+	{
+		std::cout << vec[i] << ", " << vec.length() << ", " << vec.max() << std::endl;
 	}
 
-
-	std::cin >> hello;
-
+	std::system("pause");
 }
